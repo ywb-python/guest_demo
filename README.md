1356e0e9227682f6eba02139240cac1878adcffe
18721706546@163.com <ywb1718951687>
2022-01-25 18:26:08 +0800

    date:2022/1/25 desc: 1.在视图中完成Django的用户认证

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # guest_demo
 
 该项目是基于Django框架，主要进行web接口开发以及接口自动化测试相关体系功能
+
+1.超级管理员账户
+
+username:muyi
+
+password:resetpwd
+
+email:18721706546@163.com
+
+2.账户信息
+
+账户1
+
+​	用户名:tom
+
+​	密码:tomjerry
+
+账户2
+
+​	用户名:xitele
+
+​	密码:wodefendou
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -10,14 +12,17 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get("username", "")
         password = request.POST.get("password", "")
-        if username == "admin" and password == "admin123":
-            response = HttpResponseRedirect("/event_manage/")
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
             request.session['user'] = username
+            response = HttpResponseRedirect("/event_manage/")
             return response
         return render(request, 'index.html', {"error": "username or password error!"})
 
 
+@login_required
 def event_manage(request):
     username = request.session.get('user', '')
-    return render(request, 'event_manage.html', {'user':username})
+    return render(request, 'event_manage.html', {'user': username})
 # Create your views here.
