c59178a96ecd00240785a140fd9ed5a1d13c818c
18721706546@163.com <ywb1718951687>
2022-01-25 16:10:52 +0800

    date:2022/1/25 desc: 1.登录功能的实现

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -22,5 +22,7 @@ from sign import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index$', views.index)
+    url(r'^index$', views.index),
+    url(r'^login_action', views.login_action),
+    url(r'^event_manage', views.event_manage),
 ]
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        if username == "admin" and password == "admin123":
+            return HttpResponseRedirect("/event_manage/")
+        return render(request, 'index.html', {"error": "username or password error!"})
+
+
+def event_manage(request):
+    return render(request, 'event_manage.html')
 # Create your views here.
